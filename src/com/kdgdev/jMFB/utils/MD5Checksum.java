ef39b73e09a12dd83e6c23e48a5797a51279a629
MIUI Russia <fc.miuirussia@gmail.com>
2012-09-20 19:11:14 +0400

    First commit
    Signed-off-by: MIUI Russia <fc.miuirussia@gmail.com>

---
+++ src/com/kdgdev/jMFB/utils/MD5Checksum.java
@@ -0,0 +1,46 @@
+package com.kdgdev.jMFB.utils;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kirill
+ * Date: 18.07.12
+ * Time: 22:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class MD5Checksum {
+
+    public static byte[] createChecksum(String filename) throws Exception {
+        InputStream fis = new FileInputStream(filename);
+
+        byte[] buffer = new byte[1024];
+        MessageDigest complete = MessageDigest.getInstance("MD5");
+        int numRead;
+
+        do {
+            numRead = fis.read(buffer);
+            if (numRead > 0) {
+                complete.update(buffer, 0, numRead);
+            }
+        } while (numRead != -1);
+
+        fis.close();
+        return complete.digest();
+    }
+
+    // see this How-to for a faster way to convert
+    // a byte array to a HEX string
+    public static String getMD5Checksum(String filename) throws Exception {
+        byte[] b = createChecksum(filename);
+        String result = "";
+
+        for (int i = 0; i < b.length; i++) {
+            result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+        }
+        return result;
+    }
+
+}
